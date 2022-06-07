aa3ab5fcb47dc8d29cf881355ff7266dd2af3316
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-06-06 22:16:49 -0600

    Remove un-needed page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@chakra-ui/react';
 import Home from './pages/Home';
-import Viewer from './pages/Viewer';
 import Wallets from './pages/Wallets';
 import SideMenu from './components/Menus/SideMenu';
 
 function App() {
   return (
     <Box textAlign="center" fontSize="xl">
-      <SideMenu />
       <BrowserRouter>
+        <SideMenu />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/wallets" element={<Viewer />} />
-          <Route path="/viewer" element={<Wallets />} />
+          <Route path="/wallets" element={<Wallets />} />
         </Routes>
       </BrowserRouter>
     </Box>
