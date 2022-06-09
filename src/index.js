c7f2610e6995476557009a2371e9db6948d5e771
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-06-08 21:49:23 -0600

    Wrap app in newly created Provider

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import theme from './theme/theme';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from './context/WalletContext';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
   <StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript />
-      <App />
-    </ChakraProvider>
+    <Provider>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript />
+        <App />
+      </ChakraProvider>
+    </Provider>
   </StrictMode>
 );
