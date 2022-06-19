a467d07c2c3afc42010ae4d11a9ea162bc2b3090
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-06-18 19:14:53 -0600

    Update walletContext

--- src/context/WalletContext.js
+++ src/context/WalletContext.js
@@ -1,7 +1,10 @@
 import createDataContext from './createDataContext';
+import jsonServer from '../api/jsonServer';
 
 const walletReducer = (state, action) => {
   switch (action.type) {
+    case 'get_wallets':
+      return action.payload;
     case 'add_wallet':
       return [
         ...state,
@@ -10,28 +13,52 @@ const walletReducer = (state, action) => {
           name: action.payload.name,
         },
       ];
-    case 'deleteWallet':
+    case 'edit_wallet':
+      return state.map(wallet => {
+        return wallet.id === action.payload.id ? action.payload : wallet;
+      });
+    case 'delete_wallet':
       return state.filter(entry => entry.walletId !== action.payload);
     default:
       return state;
   }
 };
 
+const getWallets = dispatch => {
+  return async () => {
+    const response = await jsonServer.get('/wallets');
+
+    dispatch({ type: 'get_wallets', payload: response.data });
+  };
+};
+
 const addWallet = dispatch => {
-  return (walletId, name) => {
-    dispatch({ type: 'add_wallet', payload: { walletId, name } });
+  return async (walletId, name) => {
+    await jsonServer.post('/wallets', { walletId, name });
+
+    dispatch({ type: 'add_wallet', payload: { id: walletId, name } });
   };
 };
 
 const deleteWallet = dispatch => {
-  return id => {
+  return async id => {
+    await jsonServer.delete(`/wallets/${id}`);
+
     dispatch({ type: 'delete_wallet', payload: id });
+  };
+};
+
+const editWallet = dispatch => {
+  return async (id, walletId, name) => {
+    await jsonServer.put(`/wallets/${id}`, { walletId, name });
+
+    dispatch({ type: 'edit_blogpost', payload: { id, walletId, name } });
   };
 };
 
 export const { Context, Provider } = createDataContext(
   walletReducer,
-  { addWallet, deleteWallet },
+  { addWallet, deleteWallet, getWallets, editWallet },
   [
     {
       walletId: '0x3beb173F2953476b9Bd303E1AAAd410590380FbC',
