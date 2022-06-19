8be29c6fbd428358d00adfe36e3bf05f04d6bb86
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-06-18 20:52:44 -0600

    Create basic walletform

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,19 +1,30 @@
+import { useState, useContext, useEffect } from 'react';
 import {
   Button,
+  Flex,
   Grid,
   GridItem,
+  Heading,
   Input,
   InputGroup,
   Text,
 } from '@chakra-ui/react';
-import { useState } from 'react';
+import { Context } from '../context/WalletContext';
 
 const WalletForm = () => {
   const [walletId, setWalletId] = useState('');
+  const [walletName, setWalletName] = useState('');
+  const { addWallet, state } = useContext(Context);
 
   const viewContentsHandler = () => {};
 
-  const addToWalletsHandler = () => {};
+  const addToWalletsHandler = async () => {
+    await addWallet(walletId, walletName);
+  };
+
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
 
   return (
     <Grid
@@ -23,19 +34,41 @@ const WalletForm = () => {
       gap="5"
     >
       <GridItem colSpan={2}>
-        <Text mb="2" textAlign="left">
-          Wallet ID:
-        </Text>
+        <Heading width="100%">Wallet Contents Viewer</Heading>
+      </GridItem>
+      <GridItem colSpan={2}>
+        <Flex gap={'5'} justifyContent={'center'} alignItems="center">
+          <InputGroup width="50%">
+            <Input
+              placeholder="wallet ID"
+              value={walletId}
+              onChange={event => setWalletId(event.target.value)}
+            />
+          </InputGroup>
+
+          <InputGroup width="50%">
+            <Input
+              placeholder="wallet Name"
+              value={walletName}
+              onChange={event => setWalletName(event.target.value)}
+            />
+          </InputGroup>
+        </Flex>
+      </GridItem>
+
+      <Button onClick={() => viewContentsHandler()}>View Contents</Button>
+      <Button onClick={() => addToWalletsHandler()}>Add to My Wallets</Button>
+
+      <GridItem colSpan={2}>
+        <Text>Currently Selected ID:</Text>
         <InputGroup>
           <Input
-            placeholder="wallet ID"
-            value={walletId}
-            onChange={event => setWalletId(event.target.value)}
+            placeholder="wallet Name"
+            value={walletName}
+            onChange={event => setWalletName(event.target.value)}
           />
         </InputGroup>
       </GridItem>
-      <Button onClick={() => viewContentsHandler()}>View Contents</Button>
-      <Button onClick={() => addToWalletsHandler()}>Add to My Wallets</Button>
     </Grid>
   );
 };
